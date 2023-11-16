37a99a285e959455e263737d2439b5161dab605a
xiang ni <xiang_x_ni@manulife.com>
2023-11-16 10:50:01 +0800

    update

--- platform/decorator.d.ts
+++ platform/decorator.d.ts
@@ -4,8 +4,11 @@ type ProvDecorator = (token: TokenKey, provider?: {
     providedIn?: string;
     [key: string]: any;
 }) => any;
+declare class PluginIntercept {
+    register(): Promise<void>;
+}
 export declare const registerProvider: (provider: Provider) => number;
-export declare const ApplicationPlugin: (this: unknown, ...args: any[]) => (cls: Type<any>) => any;
+export declare const ApplicationPlugin: (this: unknown, ...args: any[]) => (cls: Type<PluginIntercept>) => any;
 export declare const Prov: ProvDecorator;
 export declare const Input: (key: string) => any;
 export declare const execute: (applicationContext: ApplicationContext) => void;
