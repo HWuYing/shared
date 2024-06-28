9439aca3e9eb3f7f4e5a260c45cc92782c88f1c0
xiang ni <xiang_x_ni@manulife.com>
2024-06-28 14:17:17 +0800

    update

--- platform/decorator.d.ts
+++ platform/decorator.d.ts
@@ -1,15 +1,10 @@
 import { Provider, TokenKey, Type } from '@fm/di';
 import type { ApplicationContext } from './application';
-type ProvDecorator = (token: TokenKey, provider?: {
-    providedIn?: string;
-    [key: string]: any;
-}) => any;
-interface PluginIntercept {
-    register(): Promise<void>;
-}
 export declare const registerProvider: (provider: Provider) => number;
-export declare const ApplicationPlugin: <T extends PluginIntercept>(this: unknown, ...args: any[]) => (cls: Type<T>) => any;
-export declare const Prov: ProvDecorator;
+export declare const ApplicationPlugin: () => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction;
+export declare const Prov: (args_0: TokenKey, args_1?: {
+    [key: string]: any;
+    providedIn?: string;
+}) => MethodDecorator;
 export declare const Input: (key: string) => any;
 export declare const execute: (applicationContext: ApplicationContext) => void;
-export {};
